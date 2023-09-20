0820767e5fba8a81c8980920ff9633ef12c41c23
Alexandre Tchouprine <alexandret@piepacker.com>
2023-09-20 13:47:37 +0300

    color scheme change

--- init.lua
+++ init.lua
@@ -144,10 +144,11 @@ require('lazy').setup({
 
   {
     -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    'rose-pine/neovim',
+    as = 'rose-pine',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'rose-pine'
     end,
   },
 
